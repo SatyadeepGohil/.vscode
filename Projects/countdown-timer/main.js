8da05c9a98a104085af0a64284373325e21ca191
SatyadeepGohil <gohilsatyadeep7@gmail.com>
2024-05-18 17:20:41 +0530

    countdowntimer

--- Projects/countdown-timer/main.js
+++ Projects/countdown-timer/main.js
@@ -1,10 +1,11 @@
 let timer = document.getElementById("timer");
 const datetimeInput = prompt("Enter the target date and time (YYYY-MM-DDTHH:MM:SS)");
-    if (datetimeInput) {
-        const countDownDate = new Date(datetimeInput).getTime();
+
     const countdownFunction = setInterval(() => {
+    const countDownDate = new Date(datetimeInput).getTime();
     const now = new Date().getTime();
     const distance = countDownDate - now;
+
 
     const days = Math.floor(distance/(1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -17,9 +18,6 @@ const datetimeInput = prompt("Enter the target date and time (YYYY-MM-DDTHH:MM:S
         clearInterval(countdownFunction);
         timer.innerHTML = "TIME OUT";
     }
-}, 1000);
-    }
-    if (isNaN(countDownDate)) {
-        alert("Invalid date format.");
-        return;
-    }
+    }, 1000);
+
+/* 2024-05-20T15:30:00 */
